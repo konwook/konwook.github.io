127b12a669d51925edc8f383be72fd3383632de2
konwook <konwoo.kim@gmail.com>
2023-12-18 08:00:47 +0000

    Deploying to gh-pages from @ konwook/konwook.github.io@b7f267a7a5df5e62a493fd73458b08474bb19398 🚀

---
+++ _app/immutable/chunks/stores.13ad6f8a.js
@@ -0,0 +1,1 @@
+import"./index.d20aca82.js";import{s as e}from"./singletons.9e9297a7.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
